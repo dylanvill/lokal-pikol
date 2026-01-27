0eb662e44bdc2c27b0c4c176867bc8c11eee644a
Dylan Villaruel <dylan@ad-group.com.au>
2026-01-27 16:24:55 +0800

    feat: added scaffolding for court page

---
+++ resources/js/pages/court.tsx
@@ -0,0 +1,16 @@
+import { Container, Text, VStack } from '@chakra-ui/react';
+import LandingPageLayout from '@/layouts/LandingPageLayout';
+
+export default function Court() {
+    return (
+        <LandingPageLayout title="Court">
+            <Container maxW="7xl" py={8}>
+                <VStack spacing={6} align="stretch">
+                    <Text fontSize="lg" color="gray.600">
+                        Court page content will go here
+                    </Text>
+                </VStack>
+            </Container>
+        </LandingPageLayout>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
+
+Route::get('/court/{court}', function ($court) {
+    return Inertia::render('court', [
+        'court' => $court
+    ]);
+})->name('court');
