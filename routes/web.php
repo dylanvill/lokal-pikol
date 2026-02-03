21c963e8647d4ae7a74aeea51c67cb37b2ff1514
Dylan Villaruel <dylan@ad-group.com.au>
2026-02-03 09:49:37 +0800

    refactor: moved admin routes to separate and updated configuration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Admin\Controllers\AccountController;
-use App\Http\Admin\Controllers\CourtsController;
-use App\Http\Admin\Controllers\DashboardController;
-use App\Http\Admin\Controllers\ReservationsController;
 use App\Http\Controllers\CourtController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -12,9 +8,3 @@ Route::get('/', HomeController::class)->name('home');
 
 Route::get('/court/{court}', CourtController::class)->name('court');
 
-Route::prefix("admin")->group(function () {
-    Route::get('/dashboard', DashboardController::class)->name('admin.dashboard');
-    Route::get('/reservations', ReservationsController::class)->name('admin.reservations');
-    Route::get('/courts', CourtsController::class)->name('admin.courts');
-    Route::get('/account', AccountController::class)->name('admin.account');
-});
