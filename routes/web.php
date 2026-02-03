f841a5ab83304c410aef60629e07d903bcc05b33
Dylan Villaruel <dylan@ad-group.com.au>
2026-02-03 09:00:20 +0800

    feat: scaffolding for admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Admin\Controllers\AdminDashboardController;
 use App\Http\Controllers\CourtController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -7,3 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', HomeController::class)->name('home');
 
 Route::get('/court/{court}', CourtController::class)->name('court');
+
+Route::prefix("admin")->group(function () {
+    Route::get('/dashboard', AdminDashboardController::class)->name('admin.dashboard');
+});
