d6c439351321d88cfeb3d9b9fb09b56e8b9abea4
Dylan Villaruel <dylan@ad-group.com.au>
2026-02-11 11:37:02 +0800

    feat: added date format

--- resources/js/components/facility/CourtCard.tsx
+++ resources/js/components/facility/CourtCard.tsx
@@ -1,4 +1,5 @@
 import { Box, Image, Card, VStack, Badge, Text, Heading, HStack } from '@chakra-ui/react';
+import dayjs from 'dayjs';
 import courtTypeIconParser from '../../helpers/courtTypeIconParser';
 import typographyTokens from '../../lib/tokens/typography';
 import type CourtPricing from '../../models/facility/CourtPricing';
@@ -13,6 +14,16 @@ interface CourtCardProps {
 
 function CourtCard({ name, photo, covered, courtPricings }: CourtCardProps) {
     const Icon = courtTypeIconParser(covered);
+
+    const formattedCourtPricings = courtPricings.map((pricing) => {
+        const dateToday = dayjs().format('YYYY-MM-DD');
+
+        return {
+            ...pricing,
+            startTime: dayjs(`${dateToday} ${pricing.startTime}`, 'YYYY-MM-DD HH:mm:ss').format('h:mm A'),
+            endTime: dayjs(`${dateToday} ${pricing.endTime}`, 'YYYY-MM-DD HH:mm:ss').format('h:mm A'),
+        };
+    });
 
     return (
         <Card.Root borderRadius="lg" overflow="hidden">
@@ -30,7 +41,7 @@ function CourtCard({ name, photo, covered, courtPricings }: CourtCardProps) {
                         <Text fontSize={typographyTokens.small.fontSize} color={typographyTokens.small.colors.gray[500]} fontWeight="medium">
                             Court Hours:
                         </Text>
-                        {courtPricings.map((pricing) => (
+                        {formattedCourtPricings.map((pricing) => (
                             <HStack gap={0}>
                                 <Badge key={pricing.id} size="sm" fontFamily="mono" borderRightRadius={0} colorPalette="blue">
                                     {pricing.startTime} - {pricing.endTime}
