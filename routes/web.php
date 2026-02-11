d9ad8bc3e04d3ee990b6965af34c02e26114285f
Dylan Villaruel <dylan@ad-group.com.au>
2026-02-11 16:09:13 +0800

    feat: scaffolding for signing up

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Customer\Auth\Controllers\SignUpController;
 use App\Http\Customer\Facility\Controllers\FacilityController;
 use App\Http\Customer\Facility\Controllers\FacilitiesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', FacilitiesController::class)->name('home');
+Route::get('/sign-up', SignUpController::class)->name('sign-up');
 Route::get('/facilities', fn() => redirect(route("home")))->name('login');
 Route::get('/facilities/{facility:uuid}', FacilityController::class)->name('facility');
