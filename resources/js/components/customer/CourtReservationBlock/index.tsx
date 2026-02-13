ade2fdfde747b953816c4969c696bb6db7e6a293
Dylan Villaruel <dylan@ad-group.com.au>
2026-02-14 00:19:24 +0800

    feat: dynamic button display

--- resources/js/components/customer/CourtReservationBlock/index.tsx
+++ resources/js/components/customer/CourtReservationBlock/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Field, GridItem, Heading, HStack, Input, SimpleGrid, Text } from '@chakra-ui/react';
+import { Button, Field, GridItem, Heading, HStack, Input, SimpleGrid, Text, VStack } from '@chakra-ui/react';
 import { Form } from '@inertiajs/react';
 import dayjs from 'dayjs';
 import { LuArrowRight, LuSun } from 'react-icons/lu';
@@ -29,44 +29,53 @@ function CourtReservationBlock({ facilityId, courtId, name, photos, slots }: Cou
                     <Text>Outdoor Court</Text>
                 </HStack>
                 <Form action={`/facilities/${facilityId}/courts/${courtId}/reserve`} method="post">
-                    {({ submit, clearErrors }) => (
-                        <>
-                            <Field.Root marginBottom={8} maxW={{ base: 'full', md: 'sm' }}>
-                                <Field.Label htmlFor="date" as="text">
-                                    Date:
-                                </Field.Label>
-                                <Input type="date" name="date" defaultValue={today} min={today} />
-                                <Field.HelperText>Select a date to view available courts.</Field.HelperText>
-                            </Field.Root>
-                            <Text marginBottom={2}>Select time slots:</Text>
-                            <SimpleGrid columns={{ base: 2, md: 4, lg: 3, xl: 4 }} gap={4}>
-                                {slots.map((slot) => (
-                                    <CourtSlotSection
-                                        key={`${courtId}-${slot.startTime}-${slot.endTime}`}
-                                        courtId={courtId}
-                                        label={`${slot.startTime} - ${slot.endTime}`}
-                                        startTime={slot.startTime}
-                                        endTime={slot.endTime}
-                                        price={slot.price}
-                                        onSlotSelected={() => {}}
-                                        onSlotDeselected={() => {}}
-                                    />
-                                ))}
-                            </SimpleGrid>
-                            <Button
-                                type="submit"
-                                onClick={() => {
-                                    submit();
-                                    clearErrors();
-                                }}
-                                colorScheme="blue"
-                                marginTop={4}
-                            >
-                                Reserve
-                                <LuArrowRight />
-                            </Button>
-                        </>
-                    )}
+                    {({ submit, clearErrors, getData }) => {
+                        const keys = Object.keys(getData());
+                        const canBook = keys.includes('slots') && keys.includes('date');
+                        return (
+                            <>
+                                <Field.Root marginBottom={8} maxW={{ base: 'full', md: 'sm' }}>
+                                    <Field.Label htmlFor="date" as="text">
+                                        Date:
+                                    </Field.Label>
+                                    <Input type="date" name="date" defaultValue={today} min={today} />
+                                    <Field.HelperText>Select a date to view available courts.</Field.HelperText>
+                                </Field.Root>
+                                <Text marginBottom={2}>Select time slots:</Text>
+                                <SimpleGrid columns={{ base: 2, md: 4, lg: 3, xl: 4 }} gap={4}>
+                                    {slots.map((slot) => (
+                                        <CourtSlotSection
+                                            key={`${courtId}-${slot.startTime}-${slot.endTime}`}
+                                            courtId={courtId}
+                                            label={`${slot.startTime} - ${slot.endTime}`}
+                                            startTime={slot.startTime}
+                                            endTime={slot.endTime}
+                                            price={slot.price}
+                                            onSlotSelected={() => {}}
+                                            onSlotDeselected={() => {}}
+                                        />
+                                    ))}
+                                </SimpleGrid>
+                                {canBook && (
+                                    <VStack alignItems="flex-end" gap={1}>
+                                        <Button
+                                            type="submit"
+                                            onClick={() => {
+                                                submit();
+                                                clearErrors();
+                                            }}
+                                            colorScheme="blue"
+                                            marginTop={4}
+                                        >
+                                            Reserve Slots
+                                            <LuArrowRight />
+                                        </Button>
+                                        <Text fontSize="xs" color="gray.500">Proceed to book {name}</Text>
+                                    </VStack>
+                                )}
+                            </>
+                        );
+                    }}
                 </Form>
             </GridItem>
         </SimpleGrid>
