f37b5cb20d6c77335a443a074bcc658e4a0a5291
Dylan Villaruel <dylan@ad-group.com.au>
2026-02-13 15:26:38 +0800

    feat: reservation structure

--- app/Source/Reservation/Enums/ReservationStatusEnum.php
+++ app/Source/Reservation/Enums/ReservationStatusEnum.php
@@ -4,6 +4,7 @@ namespace App\Source\Reservation\Enums;
 
 enum ReservationStatusEnum: string
 {
+    case ON_HOLD = 'on_hold';
     case PENDING = 'pending';
     case CONFIRMED = 'confirmed';
     case CANCELLED = 'cancelled';
--- resources/js/pages/customer/facilities/reserve.tsx
+++ resources/js/pages/customer/facilities/reserve.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { Container } from '@chakra-ui/react';
+import ReservationForm from '../../../components/customer/ReservationForm';
+import DefaultPageLayout from '../../../layouts/DefaultPageLayout';
 
 function ReservePage() {
-    return <div>ReservePage</div>;
+    return (
+        <DefaultPageLayout title="Facility Name">
+            <Container maxW="7xl" px={4} py={4}>
+                <ReservationForm />
+            </Container>
+        </DefaultPageLayout>
+    );
 }
 
-export default ReservePage
+export default ReservePage;
