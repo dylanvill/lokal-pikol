b8d04a4d7113a09e46c03282d89686e3a3deb97e
Dylan Villaruel <dylan@ad-group.com.au>
2026-02-14 23:06:00 +0800

    refactor: directory structure

--- resources/js/pages/customer/facilities/reserve.tsx
+++ resources/js/pages/customer/facilities/reserve.tsx
@@ -1,12 +1,20 @@
-import { Container } from '@chakra-ui/react';
-import ReservationForm from '../../../components/customer/ReservationForm';
+import { Container, VStack } from '@chakra-ui/react';
+import PaymentBreakdown from '../../../components/customer/ReservationReview/PaymentBreakdown';
+import PaymentUpload from '../../../components/customer/ReservationReview/PaymentUpload';
+import ReservationDetails from '../../../components/customer/ReservationReview/ReservationDetails';
+import ReservationNotice from '../../../components/customer/ReservationReview/ReservationNotice';
 import DefaultPageLayout from '../../../layouts/DefaultPageLayout';
 
 function ReservePage() {
     return (
         <DefaultPageLayout title="Facility Name">
             <Container maxW="7xl" px={4} py={4}>
-                <ReservationForm />
+                <VStack gap={6} align="stretch">
+                    <ReservationNotice />
+                    <ReservationDetails />
+                    <PaymentBreakdown />
+                    <PaymentUpload />
+                </VStack>
             </Container>
         </DefaultPageLayout>
     );
