a27e3b4435db41f8f7c44fbb5855cb99e0630b38
Dylan Villaruel <dylan@ad-group.com.au>
2026-02-15 09:21:02 +0800

    feat: integrated payment breakdown

--- resources/js/components/customer/ReservationReview/PaymentBreakdown.tsx
+++ resources/js/components/customer/ReservationReview/PaymentBreakdown.tsx
@@ -1,5 +1,6 @@
 import { Card, HStack, Separator, Text, VStack } from '@chakra-ui/react';
 import currencyFormatter from '../../../helpers/currencyFormatter';
+import militaryTimeToAmPmTime from '../../../helpers/militaryTimeToAmPmTime';
 import type Reservation from '../../../models/customer/reservation/Reservation';
 import CardHeading from './CardHeading';
 import PaymentItem from './PaymentItem';
@@ -9,20 +10,30 @@ export interface PaymentBreakdownProps {
 }
 
 function PaymentBreakdown({ reservation }: PaymentBreakdownProps) {
+    const hourlyFees = reservation.fees.hourlyFees.map((fee) => ({
+        ...fee,
+        startTime: militaryTimeToAmPmTime(fee.startTime),
+        endTime: militaryTimeToAmPmTime(fee.endTime),
+    }));
+
+    const totalFees = reservation.fees.hourlyFees.reduce((total, fee) => total + fee.price, 0) + reservation.fees.serviceFee;
+
     return (
         <Card.Root>
             <Card.Body>
                 <CardHeading text="Payment Breakdown" />
-                <VStack gap={3}>
-                    <PaymentItem label="Court Fee (2 hours)" amount={800} />
-                    <PaymentItem label="Service Fee" amount={15} />
+                <VStack gap={2}>
+                    {hourlyFees.map((fee) => (
+                        <PaymentItem label={`${fee.startTime} - ${fee.endTime}`} amount={fee.price} />
+                    ))}
+                    <PaymentItem label="Service Fee" amount={reservation.fees.serviceFee} />
                     <Separator width="full" />
                     <HStack justify="space-between" width="full">
                         <Text fontWeight="bold" fontSize="lg">
                             Total
                         </Text>
                         <Text fontWeight="bold" fontSize="lg">
-                            {currencyFormatter(815)}
+                            {currencyFormatter(totalFees)}
                         </Text>
                     </HStack>
                 </VStack>
