2bd736fd00bf8aa23b215fb4cf0ac6b1be8d0495
Dylan Villaruel <dylan@ad-group.com.au>
2026-02-17 17:53:18 +0800

    feat: initial search implementation

--- app/Http/Customer/Facility/Controllers/FacilitiesController.php
+++ app/Http/Customer/Facility/Controllers/FacilitiesController.php
@@ -3,27 +3,33 @@
 namespace App\Http\Customer\Facility\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Customer\Court\Resources\CourtListResource;
 use App\Http\Customer\Facility\Requests\FacilitiesRequest;
 use App\Http\Customer\Facility\Resources\FacilityListResource;
 use App\Source\Facility\Models\Facility;
-use App\Source\Court\Models\Court;
 use App\Source\MediaLibrary\Enums\MediaTypeEnum;
-use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Inertia\Inertia;
 use Inertia\Response;
+
+use function Symfony\Component\Clock\now;
 
 class FacilitiesController extends Controller
 {
 
     public function __invoke(FacilitiesRequest $request): Response
     {
-        $currentHour = now()->hour;
+
+        $date = $request->date ??Carbon::now()->format('Y-m-d');
+        $startTime = $request->startTime ?? Carbon::now()->addHour()->startOfHour()->format('H:i');
+        $endTime = $request->endTime ?? Carbon::now()->addHours(3)->startOfHour()->format('H:i');
+        $city = $request->city ?? null;
 
         $facilities = Facility::with([
             'courts',
             'courts.courtPricings',
-            'courts.reservations',
+            'courts.reservations' => function ($query) use ($date) {
+                $query->whereDate('reservation_date', $date);
+            },
             'media' => function ($query) {
                 $query->whereIn('collection_name', [
                     MediaTypeEnum::FACILITY_PROFILE_PHOTO->value,
@@ -32,6 +38,22 @@ class FacilitiesController extends Controller
             }
         ])
             ->whereHas('courts')
+            ->where(function ($query) use ($date, $startTime, $endTime) {
+                $query->whereDoesntHave('reservations', function ($reservationQuery) use ($date, $startTime, $endTime) {
+                    $reservationQuery->whereDate('reservation_date', $date)
+                        ->where(function ($timeQuery) use ($startTime, $endTime) {
+                            // Check for overlapping times
+                            $timeQuery->where(function ($overlap) use ($startTime, $endTime) {
+                                // Reservation starts before our end time AND ends after our start time
+                                $overlap->where('start_time', '<', $endTime)
+                                    ->where('end_time', '>', $startTime);
+                            });
+                        });
+                });
+            })
+            ->when($city, function ($query, $city) {
+                $query->where('city', $city);
+            })
             ->paginate(15);
 
         return Inertia::render('customer/facilities', ['facilities' => Inertia::scroll(fn() => FacilityListResource::collection($facilities))]);
--- resources/js/components/customer/HomeHeader/SearchBar.tsx
+++ resources/js/components/customer/HomeHeader/SearchBar.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Field, Grid, Input, NativeSelect } from '@chakra-ui/react';
-import { Form } from '@inertiajs/react';
+import { useForm } from '@inertiajs/react';
 import dayjs from 'dayjs';
 import { LuSearch } from 'react-icons/lu';
 
@@ -8,14 +8,32 @@ function SearchBar() {
     const nextHour = dayjs().add(1, 'hour').format('HH:00');
     const endHour = dayjs().add(3, 'hour').format('HH:00');
 
+    const form = useForm({
+        city: '',
+        date: currentDate,
+        startTime: nextHour,
+        endTime: endHour,
+    });
+
+    const minEndTime = form.data.startTime ? dayjs(form.data.startTime, 'HH:mm').add(1, 'hour').format('HH:mm') : nextHour;
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        form.get('/', {
+            preserveState: true,
+            preserveScroll: true,
+        });
+    };
+
     return (
         <Box bg="white" borderRadius={16} borderColor="gray.200" p={{ base: 4, md: 4 }} shadow="sm" colorPalette="blue">
-            <Form method="get" action="/search">
+            <form onSubmit={handleSubmit}>
                 <Grid gap={4} alignItems="flex-end" gridTemplateColumns={{ base: '1fr 1fr', md: '1fr 1fr 1fr 1fr auto' }}>
                     <Field.Root>
                         <Field.Label>City</Field.Label>
                         <NativeSelect.Root>
-                            <NativeSelect.Field defaultValue="Dumaguete" name="city">
+                            <NativeSelect.Field value={form.data.city} onChange={(e) => form.setData('city', e.target.value)} name="city">
+                                <option value="">Anywhere</option>
                                 <option value="Dumaguete">Dumaguete</option>
                                 <option value="Sibulan">Sibulan</option>
                                 <option value="Valencia">Valencia</option>
@@ -26,15 +44,15 @@ function SearchBar() {
                     </Field.Root>
                     <Field.Root>
                         <Field.Label>Date</Field.Label>
-                        <Input type="date" name="date" defaultValue={currentDate} />
+                        <Input type="date" name="date" value={form.data.date} min={currentDate} onChange={(e) => form.setData('date', e.target.value)} />
                     </Field.Root>
                     <Field.Root>
                         <Field.Label>Start Time</Field.Label>
-                        <Input type="time" name="startTime" defaultValue={nextHour} />
+                        <Input type="time" name="startTime" value={form.data.startTime} min={nextHour} onChange={(e) => form.setData('startTime', e.target.value)} />
                     </Field.Root>
                     <Field.Root>
                         <Field.Label>End Time</Field.Label>
-                        <Input type="time" name="endTime" defaultValue={endHour} />
+                        <Input type="time" name="endTime" value={form.data.endTime} min={minEndTime} onChange={(e) => form.setData('endTime', e.target.value)} />
                     </Field.Root>
                     <Button
                         type="submit"
@@ -47,7 +65,7 @@ function SearchBar() {
                         Search
                     </Button>
                 </Grid>
-            </Form>
+            </form>
         </Box>
     );
 }
