5957896b5bb4f2aa02871ba4a3689150ad4ff450
Dylan Villaruel <dylan@ad-group.com.au>
2026-02-23 09:34:53 +0800

    feat: sidebar ui adjustments

--- resources/js/layouts/facility/FacilityLayout/Sidebar.tsx
+++ resources/js/layouts/facility/FacilityLayout/Sidebar.tsx
@@ -1,33 +1,53 @@
-import { Box, VStack } from '@chakra-ui/react';
+import { Box, Link, Text, VStack } from '@chakra-ui/react';
 import { router } from '@inertiajs/react';
 import { LuCalendar, LuCalendarPlus, LuGrid2X2, LuHouse, LuUser } from 'react-icons/lu';
 import AppButton from '../../../components/app/AppButton';
 
 const sidebarItems = [
     {
-        label: 'Dashboard',
-        route: '/facility/dashboard',
-        icon: () => <LuHouse />,
+        category: 'Dashboard',
+        children: [
+            {
+                label: 'Dashboard',
+                route: '/facility/dashboard',
+                icon: () => <LuHouse />,
+            },
+        ],
     },
     {
-        label: 'Courts',
-        route: '/facility/courts',
-        icon: () => <LuGrid2X2 />,
+        category: 'Courts',
+        children: [
+            {
+                label: 'Courts',
+                route: '/facility/courts',
+                icon: () => <LuGrid2X2 />,
+            },
+        ],
     },
     {
-        label: 'Reservations',
-        route: '/facility/reservations',
-        icon: () => <LuCalendar />,
+        category: 'Reservations',
+        children: [
+            {
+                label: 'Reservations',
+                route: '/facility/reservations',
+                icon: () => <LuCalendar />,
+            },
+            {
+                label: 'Create Reservation',
+                route: '/facility/reservations/create',
+                icon: () => <LuCalendarPlus />,
+            },
+        ],
     },
     {
-        label: 'Create Reservation',
-        route: '/facility/reservations/create',
-        icon: () => <LuCalendarPlus />,
-    },
-    {
-        label: 'Account',
-        route: '/facility/account',
-        icon: () => <LuUser />,
+        category: 'Account',
+        children: [
+            {
+                label: 'Account',
+                route: '/facility/account',
+                icon: () => <LuUser />,
+            },
+        ],
     },
 ];
 
@@ -38,18 +58,27 @@ export const Sidebar = () => {
 
     return (
         <Box bg="gray.100" position="fixed" p={0} left={0} top="60px" bottom={0} width="200px" overflowY="auto">
-            <VStack gap={0} align="stretch">
-                {sidebarItems.map((item) => (
-                    <AppButton
-                        key={item.route}
-                        backgroundColor="transparent"
-                        justifyContent="flex-start"
-                        borderRadius={0}
-                        onClick={() => handleItemClick(item.route)}
-                        color="black"
-                    >
-                        {item.icon()} {item.label}
-                    </AppButton>
+            <VStack gap={4} align="stretch" marginTop={4}>
+                {sidebarItems.map((category) => (
+                    <Box key={category.category}>
+                        <Text fontSize="xs" textTransform="uppercase" fontWeight="bold" color="gray.500" marginLeft={2}>
+                            {category.category}
+                        </Text>
+                        {category.children.map((item) => (
+                            <Link key={item.label}>
+                                <AppButton
+                                    key={item.route}
+                                    backgroundColor="transparent"
+                                    justifyContent="flex-start"
+                                    borderRadius={0}
+                                    onClick={() => handleItemClick(item.route)}
+                                    color="black"
+                                >
+                                    {item.icon()} {item.label}
+                                </AppButton>
+                            </Link>
+                        ))}
+                    </Box>
                 ))}
             </VStack>
         </Box>
