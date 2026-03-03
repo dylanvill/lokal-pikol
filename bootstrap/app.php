57ce8e2301d3eb419539d1e0e951002ac55a80c3
Dylan Villaruel <dylan@ad-group.com.au>
2026-03-03 12:46:01 +0800

    fix: added branding and error handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Inertia\Inertia;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -28,7 +31,20 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withCommands([
         SendOnboardingInvite::class
-    ])
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ])->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
+            if (in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+                return Inertia::render('ErrorPage', ['status' => $response->getStatusCode()])
+                    ->toResponse($request)
+                    ->setStatusCode($response->getStatusCode());
+            }
+
+            if ($response->getStatusCode() === 419) {
+                return back()->with([
+                    'message' => 'The page expired, please try again.',
+                ]);
+            }
+
+            return $response;
+        });
     })->create();
---
+++ resources/js/pages/ErrorPage.tsx
@@ -0,0 +1,45 @@
+import { Heading, Text, Container, Box, Center, Image, Separator } from '@chakra-ui/react';
+import { PiHandPeace } from 'react-icons/pi';
+import LokalPikol from '../../images/logo/lokal-pikol-horizontal-primary.svg';
+
+function ErrorPage({ status }: { status: number }) {
+    const title = {
+        503: '503: Service Unavailable',
+        500: '500: Server Error',
+        404: '404: Page Not Found',
+        403: '403: Forbidden',
+    }[status];
+
+    const description = {
+        503: 'Sorry, we are doing some maintenance. Please check back soon.',
+        500: 'Whoops, something went wrong on our servers.',
+        404: 'Sorry, the page you are looking for could not be found.',
+        403: 'Sorry, you are forbidden from accessing this page.',
+    }[status];
+
+    return (
+        <Box display="flex" flexDirection="column" minH="100vh">
+            <Container gradientFrom="blue.900" gradientTo="blue.800" bgGradient="to-t" fluid paddingTop={4} paddingBottom={20}></Container>
+            <Container py={4} shadow="2xl" borderTopRadius={20} backgroundColor="white" marginTop={-12} flex="1" maxWidth="lg" paddingTop={12}>
+                <Center>
+                    <PiHandPeace size={48} />
+                </Center>
+                <Heading textAlign="center">Don't worry</Heading>
+                <Text textAlign="center">You're seeing this because we're cooking up something in the back. We're going live soon.</Text>
+
+                <Separator marginTop={8} />
+                <Center marginTop={12} marginBottom={2}>
+                    <Image src={LokalPikol} alt="Lokal Pikol Logo" width={200} objectFit="contain" />
+                </Center>
+
+                <Text textAlign="center" fontSize="sm" fontStyle="italic" color="gray.500">
+                    A cost effective pickleball
+                    <br />
+                    booking system made in Negros Oriental
+                </Text>
+            </Container>
+        </Box>
+    );
+}
+
+export default ErrorPage;
