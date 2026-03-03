796682e19ac52b13405337c757994b6a0d6869fe
Dylan Villaruel <dylan@ad-group.com.au>
2026-03-04 07:31:13 +0800

    feat: wrapped error page in root layout

--- resources/js/pages/ErrorPage.tsx
+++ resources/js/pages/ErrorPage.tsx
@@ -1,6 +1,7 @@
 import { Heading, Text, Container, Box, Center, Image, Separator } from '@chakra-ui/react';
 import { PiHandPeace } from 'react-icons/pi';
 import LokalPikol from '../../images/logo/lokal-pikol-horizontal-primary.svg';
+import RootLayout from '../layouts/RootLayout';
 
 function ErrorPage({ status }: { status: number }) {
     const title = {
@@ -18,27 +19,29 @@ function ErrorPage({ status }: { status: number }) {
     }[status];
 
     return (
-        <Box display="flex" flexDirection="column" minH="100vh">
-            <Container gradientFrom="blue.900" gradientTo="blue.800" bgGradient="to-t" fluid paddingTop={4} paddingBottom={20}></Container>
-            <Container py={4} shadow="2xl" borderTopRadius={20} backgroundColor="white" marginTop={-12} flex="1" maxWidth="lg" paddingTop={12}>
-                <Center>
-                    <PiHandPeace size={48} />
-                </Center>
-                <Heading textAlign="center">Don't worry</Heading>
-                <Text textAlign="center">You're seeing this because we're cooking up something in the back. We're going live soon.</Text>
+        <RootLayout>
+            <Box display="flex" flexDirection="column" minH="100vh">
+                <Container gradientFrom="blue.900" gradientTo="blue.800" bgGradient="to-t" fluid paddingTop={4} paddingBottom={20}></Container>
+                <Container py={4} shadow="2xl" borderTopRadius={20} backgroundColor="white" marginTop={-12} flex="1" maxWidth="lg" paddingTop={12}>
+                    <Center>
+                        <PiHandPeace size={48} />
+                    </Center>
+                    <Heading textAlign="center">Don't worry</Heading>
+                    <Text textAlign="center">You're seeing this because we're cooking up something in the back. We're going live soon.</Text>
 
-                <Separator marginTop={8} />
-                <Center marginTop={12} marginBottom={2}>
-                    <Image src={LokalPikol} alt="Lokal Pikol Logo" width={200} objectFit="contain" />
-                </Center>
+                    <Separator marginTop={8} />
+                    <Center marginTop={12} marginBottom={2}>
+                        <Image src={LokalPikol} alt="Lokal Pikol Logo" width={200} objectFit="contain" />
+                    </Center>
 
-                <Text textAlign="center" fontSize="sm" fontStyle="italic" color="gray.500">
-                    A cost effective pickleball
-                    <br />
-                    booking system made in Negros Oriental
-                </Text>
-            </Container>
-        </Box>
+                    <Text textAlign="center" fontSize="sm" fontStyle="italic" color="gray.500">
+                        A cost effective pickleball
+                        <br />
+                        booking system made in Negros Oriental
+                    </Text>
+                </Container>
+            </Box>
+        </RootLayout>
     );
 }
 
