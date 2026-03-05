4a9a832ff851e85d63772e2fab88660023780616
Dylan Villaruel <dylan@ad-group.com.au>
2026-03-05 15:33:44 +0800

    feat: added back error page in local

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -40,7 +40,7 @@ return Application::configure(basePath: dirname(__DIR__))
         SendOnboardingInvite::class
     ])->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
-            if ( !App::environment(['local', 'staging']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
+            if ( !App::environment(['local']) && in_array($response->getStatusCode(), [500, 503, 404, 403])) {
                 return Inertia::render('ErrorPage', ['status' => $response->getStatusCode()])
                     ->toResponse($request)
                     ->setStatusCode($response->getStatusCode());
