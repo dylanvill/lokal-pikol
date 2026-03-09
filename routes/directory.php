51ad4744b0f5bc77f2a4a8a6db21952ff17fe442
Dylan Villaruel <dylan@ad-group.com.au>
2026-03-09 17:27:23 +0800

    feat: removed middleware in directory.php routes

--- routes/directory.php
+++ routes/directory.php
@@ -9,15 +9,13 @@ use App\Source\Directory\Mail\ListingRegistrationEmail;
 use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(CustomerRoutesMiddleware::class)->group(function () {
-    Route::get('/', ListingController::class)->name('home');
-    Route::get('/register', [CreateListingController::class, 'show'])->name('register.show')->middleware('signed');
-    Route::post('/register', [CreateListingController::class, 'store'])->name('register.store')->middleware(HandlePrecognitiveRequests::class);
-    Route::get('/register/{listing:uuid}/success', RegistrationSuccessController::class)->name('register.success');
-    Route::get('/privacy-policy', [ListingLegalController::class, 'privacyPolicy'])->name('privacy-policy');
-    Route::get('/terms-and-conditions', [ListingLegalController::class, 'termsAndConditions'])->name('terms-and-conditions');
+Route::get('/', ListingController::class)->name('home');
+Route::get('/register', [CreateListingController::class, 'show'])->name('register.show')->middleware('signed');
+Route::post('/register', [CreateListingController::class, 'store'])->name('register.store')->middleware(HandlePrecognitiveRequests::class);
+Route::get('/register/{listing:uuid}/success', RegistrationSuccessController::class)->name('register.success');
+Route::get('/privacy-policy', [ListingLegalController::class, 'privacyPolicy'])->name('privacy-policy');
+Route::get('/terms-and-conditions', [ListingLegalController::class, 'termsAndConditions'])->name('terms-and-conditions');
 
-    Route::get('/mail', function () {
-        return new ListingRegistrationEmail('https://find.lokal-pikol.test/register?expires=1773053155&token=qHvszYBkluqQLXrrCro6G8MjusmUJPsj&uuid=4f89983d-010a-4a41-b70d-9324c8f3b0e7&signature=928c108437d85697cedae5ddbcd76737b41113a31937b21a41c2347269cd5934');
-    });
+Route::get('/mail', function () {
+    return new ListingRegistrationEmail('https://find.lokal-pikol.test/register?expires=1773053155&token=qHvszYBkluqQLXrrCro6G8MjusmUJPsj&uuid=4f89983d-010a-4a41-b70d-9324c8f3b0e7&signature=928c108437d85697cedae5ddbcd76737b41113a31937b21a41c2347269cd5934');
 });
