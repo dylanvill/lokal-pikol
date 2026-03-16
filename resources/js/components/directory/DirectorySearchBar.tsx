243571c828bd8a45541322b769df36bc0f434399
Dylan Villaruel <dylan@ad-group.com.au>
2026-03-16 13:48:59 +0800

    feat: centralized filters

--- resources/js/components/directory/DirectorySearchBar.tsx
+++ resources/js/components/directory/DirectorySearchBar.tsx
@@ -1,7 +1,6 @@
 import { Container, Field, HStack, NativeSelect } from '@chakra-ui/react';
 import { type PageProps } from '@inertiajs/core';
-import { useForm, usePage } from '@inertiajs/react';
-import { useEffect } from 'react';
+import { usePage, router } from '@inertiajs/react';
 import invoke from '@/actions/App/Http/Directory/Controllers/ListingController';
 
 interface Item {
@@ -23,18 +22,19 @@ interface ListingPageProps extends PageProps {
 function DirectorySearchBar() {
     const { props } = usePage<ListingPageProps>();
 
-    const { data, submit, setData } = useForm({
-        city: props.filters.city || '',
-        courtType: props.filters.courtType || '',
-        numberOfCourts: props.filters.numberOfCourts || '',
-    });
-
-    useEffect(() => {
-        submit(invoke(), {
-            reset: ['listings', 'filters'],
-            preserveState: true,
-        });
-    }, [data]);
+    const handleFilterChanged = (filterKey: keyof typeof props.filters, value: string) => {
+        router.get(
+            invoke(),
+            {
+                ...props.filters,
+                [filterKey]: value || null,
+            },
+            {
+                reset: ['listings', 'filters'],
+                preserveState: true,
+            },
+        );
+    };
 
     return (
         <Container p={4} shadow="2xl" borderRadius={12} backgroundColor="white" flex="1">
@@ -50,7 +50,7 @@ function DirectorySearchBar() {
                     <Field.Root>
                         <Field.Label>City</Field.Label>
                         <NativeSelect.Root>
-                            <NativeSelect.Field value={data.city} onChange={(e) => setData('city', e.currentTarget.value)}>
+                            <NativeSelect.Field value={props.filters.city || ''} onChange={(e) => handleFilterChanged('city', e.currentTarget.value)}>
                                 {props.cities.map((city) => (
                                     <option key={city.value ?? ''} value={city.value ?? ''}>
                                         {city.label}
@@ -63,7 +63,10 @@ function DirectorySearchBar() {
                     <Field.Root>
                         <Field.Label>Court Type</Field.Label>
                         <NativeSelect.Root>
-                            <NativeSelect.Field value={data.courtType} onChange={(e) => setData('courtType', e.currentTarget.value)}>
+                            <NativeSelect.Field
+                                value={props.filters.courtType || ''}
+                                onChange={(e) => handleFilterChanged('courtType', e.currentTarget.value)}
+                            >
                                 {props.courtTypes.map((courtType) => (
                                     <option key={courtType.value ?? ''} value={courtType.value ?? ''}>
                                         {courtType.label}
@@ -76,7 +79,10 @@ function DirectorySearchBar() {
                     <Field.Root>
                         <Field.Label>Number of Courts</Field.Label>
                         <NativeSelect.Root>
-                            <NativeSelect.Field value={data.numberOfCourts} onChange={(e) => setData('numberOfCourts', e.currentTarget.value)}>
+                            <NativeSelect.Field
+                                value={props.filters.numberOfCourts || ''}
+                                onChange={(e) => handleFilterChanged('numberOfCourts', e.currentTarget.value)}
+                            >
                                 {props.numberOfCourts.map((number) => (
                                     <option key={number.value ?? ''} value={number.value ?? ''}>
                                         {number.label}
