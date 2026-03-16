8d38b8f654a52e7cba3864bc58c493cd907e7598
Dylan Villaruel <dylan@ad-group.com.au>
2026-03-16 14:15:23 +0800

    feat: added gtag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,6 +41,21 @@
     @inertiaHead
 </head>
 
+@production
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JNMXKBD68M"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-JNMXKBD68M');
+</script>
+@endproduction
+
 <body class="font-sans antialiased">
     @inertia
 </body>
