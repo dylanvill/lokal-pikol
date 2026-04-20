2b40bd2da00a295e531856dec0c164ad804f7783
Dylan Villaruel <dylan@ad-group.com.au>
2026-04-20 10:18:17 +0800

    feat: added listing ad entity

---
+++ app/Source/Directory/Models/ListingAd.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Source\Directory\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ListingAd extends Model
+{
+    //
+}
---
+++ database/migrations/2026_04_20_101324_create_listing_ads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('listing_ads', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->string('title');
+            $table->text('description');
+            $table->string('cta_url')->nullable();
+            $table->string('cta_label')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('listing_ads');
+    }
+};
