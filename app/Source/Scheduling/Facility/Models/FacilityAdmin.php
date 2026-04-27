43c26734812f21ed17567b07932055e75ce230f4
Dylan Villaruel <dylan@ad-group.com.au>
2026-04-27 14:31:05 +0800

    feat: scaffolding for facility admin

---
+++ app/Source/Scheduling/Facility/Models/FacilityAdmin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Source\Scheduling\Facility\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FacilityAdmin extends Model
+{
+    /** @use HasFactory<\Database\Factories\FacilityAdminFactory> */
+    use HasFactory;
+}
