eee5ada40d40aeff8d4df0082bd5f351235e8594
Dylan Villaruel <dylan@ad-group.com.au>
2026-05-28 12:49:34 +0800

    feat: made winning/losing team more prominent

--- resources/js/scoresheet/components/SubmitWizard/steps/SelectScoreStep.tsx
+++ resources/js/scoresheet/components/SubmitWizard/steps/SelectScoreStep.tsx
@@ -1,4 +1,4 @@
-import { Button, Heading, HStack, SimpleGrid, Text, VStack } from '@chakra-ui/react';
+import { Badge, Box, Button, Flex, Heading, HStack, SimpleGrid, Text, VStack } from '@chakra-ui/react';
 
 const SCORES = Array.from({ length: 11 }, (_, i) => i);
 
@@ -12,26 +12,31 @@ interface Props {
 function SelectScoreStep({ player1Name, player2Name, score, onSelect }: Props) {
     return (
         <VStack align="stretch" gap={4}>
-            <HStack alignItems="center" justifyContent="center" gap={2}>
-                <Heading size="xl" textAlign="center">
-                    {player1Name}
-                </Heading>
-                <Text fontSize="xl" color="gray.400">
-                    &amp;
-                </Text>
-                <Heading size="xl" textAlign="center">
-                    {player2Name}
-                </Heading>
-            </HStack>
-            <Text fontSize="sm" color="gray.500" textAlign="center">
-                Select their score:
+            <Box marginBottom={4}>
+                <Flex alignItems="center" justifyContent="center" marginBottom={1}>
+                    <Badge colorPalette="red">Losing Team</Badge>
+                </Flex>
+                <HStack alignItems="center" justifyContent="center" gap={1}>
+                    <Heading size="xl" textAlign="center">
+                        {player1Name}
+                    </Heading>
+                    <Text fontSize="xl" color="gray.400">
+                        &amp;
+                    </Text>
+                    <Heading size="xl" textAlign="center">
+                        {player2Name}
+                    </Heading>
+                </HStack>
+            </Box>
+            <Text fontSize="md" color="orange.500" textAlign="center">
+                Losing team score:
             </Text>
             <SimpleGrid columns={2} gap={2}>
                 {SCORES.map((s) => (
                     <Button
                         key={s}
                         variant={score === s ? 'solid' : 'outline'}
-                        colorPalette={score === s ? 'blue' : 'gray'}
+                        colorPalette={score === s ? 'orange' : 'gray'}
                         size="md"
                         onClick={() => onSelect(s)}
                     >
--- resources/js/scoresheet/components/SubmitWizard/steps/TeamCard.tsx
+++ resources/js/scoresheet/components/SubmitWizard/steps/TeamCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, VStack } from '@chakra-ui/react';
+import { Badge, Box, Text, VStack } from '@chakra-ui/react';
 
 interface PairInfo {
     player1Name: string;
@@ -15,34 +15,22 @@ function TeamCard({ info, won }: Props) {
     return (
         <Box
             borderWidth="1px"
-            borderColor={won ? 'green.300' : 'gray.200'}
-            bg={won ? 'green.50' : 'transparent'}
+            borderColor={won ? 'green.400' : 'orange.400'}
+            bg={won ? 'green.100' : 'orange.100'}
             borderRadius="lg"
             p={4}
             textAlign="center"
         >
+            <Badge colorPalette={won ? "green" : "red"} variant="solid" marginBottom={4}>{won ? 'Winner' : 'Loser'}</Badge>
             <VStack gap={3}>
-                <Text
-                    fontWeight="bold"
-                    fontSize="6xl"
-                    lineHeight="1"
-                    color={won ? 'green.600' : 'gray.600'}
-                >
+                <Text fontWeight="bold" fontSize="6xl" lineHeight="1" color={won ? 'green.600' : 'orange.600'}>
                     {info.score}
                 </Text>
                 <VStack gap={0}>
-                    <Text
-                        fontWeight={won ? 'semibold' : 'normal'}
-                        fontSize="sm"
-                        color={won ? 'green.700' : 'gray.600'}
-                    >
+                    <Text fontWeight={won ? 'semibold' : 'normal'} fontSize="sm" color={won ? 'green.700' : 'orange.600'}>
                         {info.player1Name}
                     </Text>
-                    <Text
-                        fontWeight={won ? 'semibold' : 'normal'}
-                        fontSize="sm"
-                        color={won ? 'green.700' : 'gray.600'}
-                    >
+                    <Text fontWeight={won ? 'semibold' : 'normal'} fontSize="sm" color={won ? 'green.700' : 'orange.600'}>
                         {info.player2Name}
                     </Text>
                 </VStack>
